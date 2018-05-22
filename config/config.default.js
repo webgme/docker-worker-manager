15970770c734bf3ac818f49ecd09b52da0fbd473
Patrik Meijer <patrik.meijer@vanderbilt.edu>
2018-05-22 11:37:58 -0500

    WIP add optional port to webgme-server, add mapping of plugin -> image

--- config/config.default.js
+++ config/config.default.js
@@ -16,9 +16,20 @@ config.server.workerManager.path = path.join(__dirname, '../dockerworkermanager'
 config.server.workerManager.options = {
     //dockerode: null, // https://github.com/apocas/dockerode#getting-started
     network: 'bridge',
-    image: 'webgme-docker-worker',
-    maxRunningContainers: 4,
-    keepContainersAtFailure: false
+    image: 'webgme-docker-worker', // By default all plugins will from this images
+    maxRunningContainers: 2,
+    keepContainersAtFailure: false,
+    // This should be a port to the webgme server on the host.
+    // If the webgme server is running on the host - the default fallback of config.server.port will work.
+    // This is needed if the webgme server itself is running inside a docker container and its port
+    // isn't map to the same host port.
+    // webgmeServerPort: <config.server.port>,
+
+    // Specific image to use for plugin. Set to null if plugin shouldn't run within docker container.
+    pluginToImage: {
+        // PluginGenerator: null,
+        // ConfigurationArtifact: 'another-image'
+    },
 };
 
 validateConfig(config);
