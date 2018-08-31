1f172b3658848ba9c40819603d41e63587462793
Patrik Meijer <patrik.meijer@vanderbilt.edu>
2018-08-31 14:41:42 -0500

    Add docker compose compatibility (#7)
    * WIP Initial setup for docker-compose compatibility
    
    * WIP Add webgme-server as dep
    
    * WIP Confirmed to work with webgme
    
    * Set config back to webgme-engine

--- config/config.default.js
+++ config/config.default.js
@@ -15,21 +15,33 @@ config.server.workerManager.path = path.join(__dirname, '../dockerworkermanager'
 // These are the default options - this section can be left out..
 config.server.workerManager.options = {
     //dockerode: null, // https://github.com/apocas/dockerode#getting-started
-    network: 'bridge',
+
     image: 'webgme-docker-worker', // By default all plugins will from this images
     maxRunningContainers: 2,
     keepContainersAtFailure: false,
-    // This should be a port to the webgme server on the host.
-    // If the webgme server is running on the host - the default fallback of config.server.port will work.
-    // This is needed if the webgme server itself is running inside a docker container and its port
-    // isn't map to the same host port.
-    // webgmeServerPort: <config.server.port>,
-
     // Specific image to use for plugin. Set to null if plugin shouldn't run within docker container.
     pluginToImage: {
         // PluginGenerator: null,
         // ConfigurationArtifact: 'another-image'
     },
+
+    // The docker network used. By default containers are running within the bridge network and
+    // can access the host at that network. Unless webgmeUrl is specified the dockerworkermanager can
+    // figure out the host IP by querying the network. If using another network than bridge - this
+    // process only works if the host is accessible from that network. (Consider using webgmeUrl for more
+    // restricted networks.
+    network: 'bridge',
+
+    // If the webgme server is running on the host - the default fallback of config.server.port will work.
+    // This can be needed if the webgme server itself is running inside a docker container and its port
+    // isn't map to the same host port. It typically requires the default bridge network to be used.
+    webgmeServerPort: null,
+
+    // If specified will be used as the full webgme server url from the perspective of a
+    // running docker worker.
+    // This is needed when connecting to the webgme server container directly (without going through the host).
+    // When defined  the network and webgmeServerPort options aren't used.
+    webgmeUrl: null,
 };
 
 validateConfig(config);
---
+++ config/config.docker.js
@@ -0,0 +1,11 @@
+/* globals process, require, module */
+const config = require('./config.default');
+
+// This is only for testing - it will persist everything inside the containers.
+config.mongo.uri = 'mongodb://' + process.env.MONGO_IP + ':27017/multi';
+config.server.workerManager.options.webgmeUrl = 'http://' + process.env.WEBGME_IP + ':' + config.server.port;
+config.server.workerManager.options.image = 'docker-worker-manager_webgme-docker-worker';
+
+
+//validateConfig(config);
+module.exports = config;
