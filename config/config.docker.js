eff86a3406322f9151481910cc6ce5399e36b1dc
Patrik Meijer <patrik.meijer@vanderbilt.edu>
2018-08-31 15:48:51 -0500

    Add option createParams to allow setting/overwriting container parameters (#8)
    * Add option createParams to allow setting/overwriting container parameters
    
    * WIP Add an example project in docker config
    
    * WIP Add missing rights

--- config/config.docker.js
+++ config/config.docker.js
@@ -5,7 +5,18 @@ const config = require('./config.default');
 config.mongo.uri = 'mongodb://' + process.env.MONGO_IP + ':27017/multi';
 config.server.workerManager.options.webgmeUrl = 'http://' + process.env.WEBGME_IP + ':' + config.server.port;
 config.server.workerManager.options.image = 'docker-worker-manager_webgme-docker-worker';
+config.server.workerManager.options.createParams = {
+    HostConfig: {
+        Memory: 536870912
+    }
+};
 
+config.seedProjects.createAtStartup = [{
+    projectName: 'Example',
+    seedId: 'EmptyProject',
+    creatorId: 'guest',
+    rights: {}
+}];
 
 //validateConfig(config);
 module.exports = config;
